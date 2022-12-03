2e9afec4b1481f362e247974eb07853950bf9bfd
SreeKuttan_ <sreekuttan@techgentsia.com>
2022-12-03 20:01:58 +0530

    Initial Commit

---
+++ WebRTC-Demo-SwiftUI/WebRTC_Demo_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  WebRTC_Demo_SwiftUIApp.swift
+//  WebRTC-Demo-SwiftUI
+//
+//  Created by Sreekuttan D on 03/12/22.
+//
+
+import SwiftUI
+
+@main
+struct WebRTC_Demo_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
